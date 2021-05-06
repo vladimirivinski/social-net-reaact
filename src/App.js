78aabd5f45dc891edca2501390e862adef767064
Vladimir <vladimir.ivinski89@gmail.com>
2021-05-06 15:43:53 +0300

    add react-router-dom, add dilog link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import Dialog from './components/Dialog/Dialog';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />,
-      <Navbar />,
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/message' component={Dialog} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Dialog/Dialog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Dialog.module.css';
+
+const Dialog = () => {
+  return (
+    <div>
+      <div className={s.item}>Dialog</div>
+    </div>
+  );
+};
+
+export default Dialog;
