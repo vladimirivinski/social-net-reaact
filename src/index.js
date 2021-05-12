18eb9e86bd8a8a7f27fea883825e898d18d3bbbd
Vladimir <vladimir.ivinski89@gmail.com>
2021-05-12 16:45:20 +0300

    refactored code and added observer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import state from './redux/state';
-import { rerenderEntireTree } from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import state, { addPost, updateNewPostText, subscribe } from './redux/state';
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
+};
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,4 @@
-import { rerenderEntireTree } from '../render';
+let rerenderEntireTree = () => {};
 
 let state = {
   dialogData: {
@@ -30,7 +30,7 @@ let state = {
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 4,
     name: state.profileData.newPostText,
@@ -41,9 +41,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profileData.newPostText = newText;
   rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
 };
 
 export default state;
