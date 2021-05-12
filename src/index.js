df79fce69fb59c95d99ed2593bc29a06c3283560
Vladimir <vladimir.ivinski89@gmail.com>
2021-05-12 18:09:05 +0300

    Rebuild index js and state to store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, { addPost, updateNewPostText, subscribe } from './redux/state';
+import store from './redux/state';
 
 let rerenderEntireTree = (state) => {
-  ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
+  ReactDOM.render(<App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />, document.getElementById('root'));
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,59 @@
-let rerenderEntireTree = () => {};
+let store = {
+  _state: {
+    dialogData: {
+      users: [
+        { id: 1, name: 'Alex' },
+        { id: 2, name: 'Max' },
+        { id: 3, name: 'Nick' },
+        { id: 4, name: 'Mike' },
+      ],
 
-let state = {
-  dialogData: {
-    users: [
-      { id: 1, name: 'Alex' },
-      { id: 2, name: 'Max' },
-      { id: 3, name: 'Nick' },
-      { id: 4, name: 'Mike' },
-    ],
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'WTF' },
+        { id: 3, message: 'Why' },
+        { id: 4, message: 'No' },
+        { id: 5, message: 'Bye' },
+      ],
+    },
 
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'WTF' },
-      { id: 3, message: 'Why' },
-      { id: 4, message: 'No' },
-      { id: 5, message: 'Bye' },
-    ],
+    profileData: {
+      post: [
+        { id: 1, name: 'First post', likeCount: 12 },
+        { id: 2, name: 'Thecond post', likeCount: 2 },
+        { id: 3, name: 'Third post', likeCount: 5 },
+      ],
+      newPostText: 'Enter your post',
+    },
   },
 
-  profileData: {
-    post: [
-      { id: 1, name: 'First post', likeCount: 12 },
-      { id: 2, name: 'Thecond post', likeCount: 2 },
-      { id: 3, name: 'Third post', likeCount: 5 },
-    ],
-    newPostText: 'Enter your post',
+  getState() {
+    return this._state;
+  },
+
+  _callSubscriber() {},
+
+  addPost() {
+    let newPost = {
+      id: 4,
+      name: this._state.profileData.newPostText,
+      likeCount: 0,
+    };
+    this._state.profileData.post.push(newPost);
+    this._state.profileData.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+
+  updateNewPostText(newText) {
+    this._state.profileData.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-window.state = state;
+window.store = store;
 
-export const addPost = () => {
-  let newPost = {
-    id: 4,
-    name: state.profileData.newPostText,
-    likeCount: 0,
-  };
-  state.profileData.post.push(newPost);
-  state.profileData.newPostText = '';
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profileData.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
