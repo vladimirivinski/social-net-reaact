91b2436ade2364bef633d48bbcfffab835b35269
Vladimir <vladimir.ivinski89@gmail.com>
2021-06-08 17:15:04 +0300

    added constructor and refactoried Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,17 @@ import * as axios from 'axios'
 import emptyPhoto from '../../assets/images/empty.jpeg'
 
 class Users extends React.Component {
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
-                this.props.setUsers(response.data.items)
-            })
-        }
+    constructor(props) {
+        super(props)
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+            this.props.setUsers(response.data.items)
+        })
     }
+
     render() {
         return (
             <div>
-                <button onClick={this.getUsers}>getUsers</button>
                 {this.props.users.map((u) => (
                     <div key={u.id}>
                         <span>
