8fb96619bd20d63466f51bc72d00bc2c3fb208c4
Vladimir <vladimir.ivinski89@gmail.com>
2021-06-08 17:30:50 +0300

    remove  constructor and added didMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,7 @@ import * as axios from 'axios'
 import emptyPhoto from '../../assets/images/empty.jpeg'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props)
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
             this.props.setUsers(response.data.items)
         })
